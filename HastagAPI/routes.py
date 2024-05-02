544eedd3667cc3b7b84dc7d6551b53952d51bb04
Carlos-Inarra <carlosinarra17@gmail.com>
2024-05-02 16:54:36 -0300

    kakakakajdhjfdidj

--- HastagAPI/routes.py
+++ HastagAPI/routes.py
@@ -12,7 +12,7 @@ class User(db.Model):
     senha = db.Column(db.String(80), nullable=False)
     token = db.Column(db.String(15),nullable=False)
 
-class Webhook(db.Model):
+class Webhooks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Evento = db.Column(db.String(50), nullable=False)
     Tempo = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
@@ -24,16 +24,16 @@ def Webhooks():
     Nome,Email,Status = Dados["nome"],Dados["email"],Dados["status"]
     match Status:
         case 'aprovado':
-            log = Webhook(Evento=f"Sistema Liberou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
+            log = Webhooks(Evento=f"Sistema Liberou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
             print("Seja bem vindo Impressionador(a)!")
         case 'recusado':
-            log = Webhook(Evento=f"Sistema Recusou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
+            log = Webhooks(Evento=f"Sistema Recusou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
             print("Pagamento recusado.")
         case 'reembolsado':
-            log = Webhook(Evento=f"Sistema Retirou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
+            log = Webhooks(Evento=f"Sistema Retirou o acesso do cliente {Nome} que possui o email:{Email}!",Dados=Dados)
             Acesso = None
         case _:
-            log = Webhook(Evento=f"Falha na tratativa",Dados=Dados)
+            log = Webhooks(Evento=f"Falha na tratativa",Dados=Dados)
             print(f"Status do pagamento desconhecido: {Status}")
     db.session.add(log)
     db.session.commit()
@@ -45,7 +45,7 @@ def Cadastro():
     else:
         Token = str(request.form["token"])
         if Token != "uhdfaAADF123":
-            log = Webhook(Evento=f"Sistema negou o cadastro de um usuario que utilizou um token diferente do permitido!",dados=f"Token utilizado{Token}")
+            log = Webhooks(Evento=f"Sistema negou o cadastro de um usuario que utilizou um token diferente do permitido!",dados=f"Token utilizado{Token}")
             db.session.add(log)
             db.session.commit()
             return "Acesso Negado"
@@ -56,7 +56,7 @@ def Cadastro():
             user = User(Usuario=Usuario, email=Email, senha=Senha,token = Token)
             db.session.add(user)
             db.session.commit()
-            log = Webhook(Evento=f"Usuario cadastrado com sucesso!",dados=f"Usuario:{Usuario},Email:{Email}")
+            log = Webhooks(Evento=f"Usuario cadastrado com sucesso!",dados=f"Usuario:{Usuario},Email:{Email}")
             db.session.add(log)
             db.session.commit()
             redirect(url_for("Login"))
@@ -72,18 +72,18 @@ def Login():
         except:
             usuarioDB = User.query.filter_by(Usuario=usuarioForm).first()
         if str(usuarioForm).lower().replace(" ","") != str(usuarioDB).lower().replace(" ",""):
-            log = Webhook(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
+            log = Webhooks(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
             db.session.add(log)
             db.session.commit()
             return render_template("LoginUsuario.html",Informacao = "Usuario Não Cadastrado")
         elif hash(request.form["senha"]) != hash(User.query.filter_by(senha=usuarioForm).first()):
-            log = Webhook(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
+            log = Webhooks(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
             db.session.add(log)
             db.session.commit()
             return render_template("LoginUsuario.html",Informacao = "Senha Incorreta")
         elif hash(request.form["senha"]) == hash(User.query.filter_by(senha=usuarioForm).first()) and str(
             usuarioForm).lower().replace(" ","") == str(usuarioDB).lower().replace(" ",""):
-            log = Webhook(Evento=f"Usuario logado com suceso!",dados=f"{usuarioForm}")
+            log = Webhooks(Evento=f"Usuario logado com suceso!",dados=f"{usuarioForm}")
             db.session.add(log)
             db.session.commit()
             return redirect("BancoDeDados")

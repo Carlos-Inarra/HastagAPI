c4e7858bbdd50c8f0e08595b1e8d77e446ac1eb2
Carlos-Inarra <carlosinarra17@gmail.com>
2024-05-02 17:02:32 -0300

    ajmajkqnmqujq

--- HastagAPI/__init__.py
+++ HastagAPI/__init__.py
@@ -6,6 +6,4 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY']='f3325d12c1d92ajqmsd9c8b41d4a'
 
-with app.app_context():
-    db.create_all()
 from HastagAPI import routes
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from doctest import debug
-from HastagAPI import app
+from HastagAPI import app,db
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

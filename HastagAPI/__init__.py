429013bcb44a5584610896f87c9747a2e46119c2
Carlos-Inarra <carlosinarra17@gmail.com>
2024-05-02 16:12:25 -0300

    jajbajhscu

--- HastagAPI/__init__.py
+++ HastagAPI/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='f3325d12c1df8cd9c8b41d4a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.config['SECRET_KEY']='f3325d12c1d92ajqmsd9c8b41d4a'
+
 
 from HastagAPI import routes
--- HastagAPI/routes.py
+++ HastagAPI/routes.py
@@ -1,21 +1,101 @@
-from flask import render_template,request,session,jsonify
-from HastagAPI import app
+from flask import redirect, render_template,request,session,jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from HastagAPI import app,db 
 from requests import get, post
-import json
+import json,hash,datetime
 
-def Enviar_mensagem(MSG):
-    token = '6268128320:AAEo1u3Ib1Z5wc2o6MifNbIeP03CxXcuf54'
-    url = f'https://api.telegram.org/bot{token}/SendMessage'
-    id = 5127620212
-    MSG = str(MSG)
-    data = {'chat_id':id,'text':MSG}
-    post(url,data)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Usuario = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(80), nullable=False)
+    token = db.Column(db.String(15),nullable=False)
+
+class Webhook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Evento = db.Column(db.String(50), nullable=False)
+    Tempo = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    Dados = db.Column(db.String(250))
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 @app.route("/EntradaDeWebhooks",methods=["POST"])
 def Webhooks():
-    dados = json.loads(request.data)
-    Enviar_mensagem(dados)
-    return "Hello"
+    Dados = json.loads(request.data)
+    match Dados["status"]:
+        case 'aprovado':
+            log = Webhook(Evento=f"Sistema Liberou o acesso do cliente {Dados["nome"]} que possui o email:{Dados["email"]}!",Dados=Dados)
+            print("Seja bem vindo Impressionador(a)!")
+        case 'recusado':
+            log = Webhook(Evento=f"Sistema Recusou o acesso do cliente {Dados["nome"]} que possui o email:{Dados["email"]}!",Dados=Dados)
+            print("Pagamento recusado.")
+        case 'reembolsado':
+            log = Webhook(Evento=f"Sistema Retirou o acesso do cliente {Dados["nome"]} que possui o email:{Dados["email"]}!",Dados=Dados)
+            Acesso = None
+        case _:
+            log = Webhook(Evento=f"Falha na tratativa",Dados=Dados)
+            print(f"Status do pagamento desconhecido: {Dados["status"]}")
+    db.session.add(log)
+    db.session.commit()
+
+@app.route("/Cadastro",methods=["POST","GET"])
+def Cadastro():
+    if request.method == "GET":
+        return render_template("CadastroUsuario.html")
+    else:
+        Token = str(request.form["token"])
+        if Token != "uhdfaAADF123":
+            log = Webhook(Evento=f"Sistema negou o cadastro de um usuario que utilizou um token diferente do permitido!",dados=f"Token utilizado{Token}")
+            db.session.add(log)
+            db.session.commit()
+            return "Acesso Negado"
+        else:
+            Usuario = str( request.form["usuario"])
+            Senha = hash(request.form["Senha"])
+            Email = str(request.form["email"])
+            user = User(Usuario=Usuario, email=Email, senha=Senha,token = Token)
+            db.session.add(user)
+            db.session.commit()
+            log = Webhook(Evento=f"Usuario cadastrado com sucesso!",dados=f"Usuario:{Usuario},Email:{Email}")
+            db.session.add(log)
+            db.session.commit()
+            redirect(url_for("Login"))
+
+@app.route("/Login",methods=["POST","GET"])
+def Login():
+    if request.method == "GET":
+        return render_template("LoginUsuario.html")
+    else:
+        usuarioForm = request.form["email_ou_usuario"]
+        try:
+            usuarioDB = User.query.filter_by(email=usuarioForm).first()
+        except:
+            usuarioDB = User.query.filter_by(Usuario=usuarioForm).first()
+        if str(usuarioForm).lower().replace(" ","") != str(usuarioDB).lower().replace(" ",""):
+            log = Webhook(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
+            db.session.add(log)
+            db.session.commit()
+            return render_template("LoginUsuario.html",Informacao = "Usuario Não Cadastrado")
+        elif hash(request.form["senha"]) != hash(User.query.filter_by(senha=usuarioForm).first()):
+            log = Webhook(Evento=f"Tentativa de login sem suceso!",dados=f"{usuarioForm}")
+            db.session.add(log)
+            db.session.commit()
+            return render_template("LoginUsuario.html",Informacao = "Senha Incorreta")
+        elif hash(request.form["senha"]) == hash(User.query.filter_by(senha=usuarioForm).first()) and str(
+            usuarioForm).lower().replace(" ","") == str(usuarioDB).lower().replace(" ",""):
+            log = Webhook(Evento=f"Usuario logado com suceso!",dados=f"{usuarioForm}")
+            db.session.add(log)
+            db.session.commit()
+            return redirect("BancoDeDados")
+
+
+@app.route("/BancoDeDados",methods=["POST","GET"])
+def BancoDeDados():
+    return render_template("TelaDB.html")
+
 
     
 

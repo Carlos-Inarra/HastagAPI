82339a85824d7f323bc2054dc442379b6a996d4d
Carlos-Inarra <carlosinarra17@gmail.com>
2024-05-02 10:53:41 -0300

    Hastagkama

--- HastagAPI/routes.py
+++ HastagAPI/routes.py
@@ -6,13 +6,16 @@ def Enviar_mensagem(MSG):
     token = '6268128320:AAEo1u3Ib1Z5wc2o6MifNbIeP03CxXcuf54'
     url = f'https://api.telegram.org/bot{token}/SendMessage'
     id = 5127620212
+    MSG = str(MSG)
     data = {'chat_id':id,'text':MSG}
     post(url,data)
 
 @app.route("/webhooks",methods=["GET","POST"])
 def EstacaZero():
     dados = request.json
+    print(dados)
     Enviar_mensagem(dados)
+    return "Hello"
 
     
 

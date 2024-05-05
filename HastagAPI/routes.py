01193e827cd12d296d6443aa5707d63d7199b350
Carlos-Inarra <carlosinarra17@gmail.com>
2024-05-05 18:24:12 -0300

    login

--- HastagAPI/routes.py
+++ HastagAPI/routes.py
@@ -1,9 +1,10 @@
-from flask import redirect, render_template,request,session,jsonify, url_for
+from flask import redirect, render_template,request,session, url_for,make_response
 from flask_sqlalchemy import SQLAlchemy
 from HastagAPI import app,db 
 import json
 from datetime import datetime
-import time
+from time import sleep
+from functools import wraps
  
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +18,22 @@ class Webhooks(db.Model):
     Evento = db.Column(db.String(250), nullable=False)
     Tempo = db.Column(db.DateTime, nullable=False, default=datetime.now())
     Dados = db.Column(db.String(250))
+    Usuario = db.Column(db.String(25))
+
+def Logado(funcao_original):
+    @wraps(funcao_original)
+    def wrapper(*args, **kwargs):
+        try:
+            a = session["usuario"]
+        except:
+            a = ""
+        if a:
+            resultado = funcao_original(*args, **kwargs)
+        else:
+            return redirect(url_for("Login"))
+        return resultado
+    return wrapper
+
 
 @app.route("/EntradaDeWebhooks",methods=["POST"])
 def Webhook():
@@ -28,31 +45,36 @@ def Webhook():
     if Dados == None:
         return None
     else:
-        Nome,Email,Status = Dados["nome"],Dados["email"],Dados["status"]
+        Nome,Email,Status,Usuario= Dados["nome"],Dados["email"],Dados["status"],Dados["nome"]
         match Status:
             case 'aprovado':
                 log = Webhooks()
                 log.Evento=f"Sistema Liberou o acesso do cliente {Nome} que possui o email:{Email}!"
-                log.Dados=str(Dados)
+                log.Dados=str(Dados).replace("{","").replace("}","")
+                log.Usuario = Usuario
                 print("Seja bem vindo Impressionador(a)!")
             case 'recusado':
                 log = Webhooks()
                 log.Evento=f"Sistema Recusou o acesso do cliente {Nome} que possui o email:{Email}!"
-                log.Dados=str(Dados)
+                log.Dados=str(Dados).replace("{","").replace("}","")
+                log.Usuario = Usuario
                 print("Pagamento recusado.")
             case 'reembolsado':
                 log = Webhooks()
                 log.Evento=f"Sistema Retirou o acesso do cliente {Nome} que possui o email:{Email}!"
-                log.Dados=str(Dados)
+                log.Dados=str(Dados).replace("{","").replace("}","")
                 Acesso = None
+                log.Usuario = Usuario
             case _:
                 log = Webhooks()
                 log.Evento=f"Falha na tratativa"
-                log.Dados=str(Dados)
+                log.Dados=str(Dados).replace("{","").replace("}","")
+                log.Usuario = Usuario
                 print(f"Status do pagamento desconhecido: {Status}")
         db.session.add(log)
         db.session.commit()
         return "Dados Recebidos"
+
 
 @app.route("/Cadastro",methods=["POST","GET"])
 def Cadastro():
@@ -64,7 +86,7 @@ def Cadastro():
             return "Acesso Negado"
         elif User.query.filter_by(email=(str(request.form["email"]).lower())).first() != None:
             render_template("CadastroUsuario.html",Informacao = "Email já cadastrado!, você será redirecionado para a tela de login")
-            time.sleep(5)
+            sleep(5)
             return redirect(url_for("Login"))
         else:
             Usuario = str( request.form["usuario"])
@@ -92,16 +114,18 @@ def Login():
                 return render_template("LoginUsuario.html",Informacao = "Senha Incorreta")
             elif str(senha) == str(usuarioDB.senha) and str(
                 usuarioForm).lower().replace(" ","") == str(usuarioDB.email).lower().replace(" ",""):
+                resp = make_response("Sucess")
+                resp.set_cookie('Usuario', 'Carlos Inarra')
                 return redirect(url_for("BancoDeDados"))
         else:
             return render_template("LoginUsuario.html",Informacao = "Usuario Não Cadastrado")
 
-
+@Logado
 @app.route("/BancoDeDados",methods=["POST","GET"])
 def BancoDeDados():
     Logs = []
     for i in Webhooks.query.all():
-        Logs.append((i.Evento,i.Dados,str(i.Tempo)[:7],i.id) )
+        Logs.append((i.Usuario,i.Evento,i.Dados,str(i.Tempo)[:-7],i.id) )
     return render_template("TelaDB.html",Logs=Logs)
 
 
